6ac916a0c2fe928d02a18b62713dffe61551ec3f
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-08-14 17:03:26 +0900

    Initial Commit

---
+++ InAppPurchaseTest/InAppPurchaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseViewController.h
+//  InAppPurchaseTest
+//
+//  Created by おかやん on 2014/08/14.
+//  Copyright (c) 2014年 ナノソフトウェア. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InAppPurchaseViewController : UIViewController
+
+@end
---
+++ InAppPurchaseTest/InAppPurchaseViewController.m
@@ -0,0 +1,29 @@
+//
+//  InAppPurchaseViewController.m
+//  InAppPurchaseTest
+//
+//  Created by おかやん on 2014/08/14.
+//  Copyright (c) 2014年 ナノソフトウェア. All rights reserved.
+//
+
+#import "InAppPurchaseViewController.h"
+
+@interface InAppPurchaseViewController ()
+
+@end
+
+@implementation InAppPurchaseViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
